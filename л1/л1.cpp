d9479746f9da6311b23fd6f2bdd42caf27bcb71f
User54681 <anzolotylh@gmail.com>
2023-09-24 16:58:38 +0300

    сохр 1ч 1з

--- л1/л1.cpp
+++ л1/л1.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <vector>
+#include <string>
 
 using std::cout;
 using std::cin;
@@ -9,10 +10,15 @@ using std::string;
 1) strlen - string::length
 2) strcpy - аналога нет, пишем a = b
 3) strcmp - compare()
-4) strcat - 
-5) someString[index] -  (getting the character at index)
-6) - push_back
-7) - pop_back
+4) strcat - ___.append() или str1 + str2
+5) someString[index] -  someString.at(index) (getting the character at index)
+6) аналога нет(можно использовать strcat) - push_back
+7) аналога нет - pop_back
+
+Функции могут не иметь точного аналога в библиотеке другого языка.Почему ? – объясните главную разницу строк в С и С++.
+
+Строки в C - массив элементов char с null-байтом в окончании
+Строки в C++ - объект класса String с динамической памятью
 */
 
 int main()
