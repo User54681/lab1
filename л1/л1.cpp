5654fb8ec5df4c0cca63409f5b0f6030adff6e1c
User54681 <anzolotylh@gmail.com>
2023-09-24 14:52:52 +0300

    правка

--- л1/л1.cpp
+++ л1/л1.cpp
@@ -1,4 +1,19 @@
 ﻿#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::cin;
+using std::string;
+
+/*
+1) strlen - 
+2) strcpy - 
+3) strcmp - 
+4) strcat - 
+5) someString[index] -  (getting the character at index)
+6) - push_back
+7) - pop_back
+*/
 
 int main()
 {
