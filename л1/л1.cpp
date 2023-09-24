2e60a640a30e71c8963b65fc73a7039bc359932c
User54681 <anzolotylh@gmail.com>
2023-09-24 19:28:06 +0300

    сохранение: з1б з2А готово

--- л1/л1.cpp
+++ л1/л1.cpp
@@ -7,7 +7,9 @@ using std::cin;
 using std::string;
 
 string task1();
-string task2();
+//string task2();
+bool check(char s, string signs);
+string sorting(string newstr);
 
 /*
 1 задание часть А:
@@ -25,20 +27,56 @@ string task2();
     Строки в C++ - объект класса String с динамической памятью
 
 2 задание часть А:
-    
+    1) int* ptr = (int*)malloc(sizeof(int)) - int* ptr = new int
+    2) int* ptrArr = (int*)malloc(sizeof(int) * N) - int* ptrArr = new int[N]
+    3) free(ptr) - delete ptr
+    4) free(ptrArr) - delete[] ptrArr
+    В С++ необходимо ли освобождать динамическую память? Да, оператором delete.
+    Можно ли освободить одну и ту же выделенную память дважды? Нет, это приводит к недокументированному поведению.
+    А можно ли освободить невыделенную память? Нет.
 */
 
 int main()
 {
     task1();
-    task2();
+    //task2();
 }
 
 
 string task1() {
+    string str;
+    string newstr;
+    cout << "enter the string: ";
+    cin >> str;
 
+    string signs = "! @ # $ % ^ & * ( ) _ + - = | \ / ? . , : ;";
+    for (int i = 0; i < str.length(); ++i) {
+        if (!check(str[i], signs)) newstr += str[i];
+    }
+    
+    cout << sorting(newstr);
+    return sorting(newstr);
 }
 
-string task2() {
-
+bool check(char s, string signs) {
+    for (int i = 0; i < signs.length(); ++i) if (s == signs[i]) return true;
+    return false;
 }
+
+string sorting(string newstr) {
+    string newstr2 = newstr;
+    for (int i = 0; i < newstr2.length(); ++i) {
+        for (int j = 0; j < newstr2.length() - i - 1; ++j) {
+            if (newstr2[j] < newstr2[j + 1]) continue;
+            char box = newstr2[j];
+            newstr2[j] = newstr2[j + 1];
+            newstr2[j + 1] = box;
+        }
+    }
+
+    return newstr2;
+}
+
+//string task2() {
+//
+//}
