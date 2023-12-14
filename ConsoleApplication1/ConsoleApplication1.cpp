aef72caf370dce1c4b71bd51ff46e794f2a904c8
User54681 <anzolotylh@gmail.com>
2023-12-15 02:05:27 +0300

    done

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::vector;
+
+int main() {
+	int n, m, q, type, card;
+	char player;
+
+	cin >> n >> m >> q;
+
+	int* a = new int[n + q] {};
+	int* b = new int[m + q] {};
+	int* c = new int[q]{};
+
+	for (int i = 0; i < n; ++i) {
+		cin >> a[i];
+	}
+
+	for (int i = 0; i < m; ++i) {
+		cin >> b[i];
+	}
+
+	for (int i = 0; i < q; ++i) {
+		int* d = new int[n + m + 2*q];
+		cin >> type >> player >> card;
+		if (type == 1) {
+			if (player == 'a') a[n + i] = card;
+			else b[n + i] = card;
+		}
+		if (type == -1) {
+			if (player == 'a') for (int j = 0; j < n + q; ++j) if (a[j] == card) a[j] = 0;
+			if (player =='b') for (int j = 0; j < m + q; ++j) if (b[j] == card) b[j] = 0;
+		}
+		for (int i = 0; i < n + q; ++i) {
+			d[i] = a[i];
+		}
+		for (int i = n+q; i < n + m + 2*q; ++i) {
+			d[i] = b[i];
+		}
+		int cnt = 0;
+		for (int i = 0; i < n + m + 2 * q; ++i) {
+			bool check = true;
+			for (int j = i + 1; j < n + m + 2 * q; ++j) if (d[i] == d[j]) check = false;
+			if (check) cnt++;
+		}
+		delete[]d;
+		c[i] = cnt;
+	}
+
+	for (int i = 0; i < q; ++i) cout << c[i] << " ";
+
+	delete[]a;
+	delete[]b;
+	delete[]c;
+}
+
---
+++ мкп2/мкп2.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+
+const double e = 0.656722629;
+const long double Pi = 3.14159265358;
+const double eps = 0.00001;
+const double a = 7689700;
+const double mu = 0.33 * pow(10, 24) * 6.67430 * pow(10, -20);
+
+double radFind(double teta, double p)
+{
+    return p / (1 + e * cos(teta));
+}
+
+double speedRad(double p, double teta)
+{
+    return sqrt(mu / p) * e * sin(teta);
+}
+
+double speedN(double p, double teta)
+{
+    return sqrt(mu / p) * (1 + e * cos(teta));
+}
+
+double fullSpeed(double p, double teta)
+{
+    double rad = speedRad(p, teta);
+    double n = speedN(p, teta);
+    return sqrt(rad * rad + n * n);
+}
+
+double excentricToTrue(double E)
+{
+    if (atan(sqrt((1 + e) / (1 - e)) * tan(E / 2)) * 2 > 0)
+    {
+        return atan(sqrt((1 + e) / (1 - e)) * tan(E / 2)) * 2;
+    }
+    else
+    {
+        return atan(sqrt((1 + e) / (1 - e)) * tan(E / 2)) * 2 + 2 * Pi;
+    }
+}
+
+double iterationMethod(double Enext, double Enow, double M)
+{
+
+    if (fabs(Enow - Enext) < eps)
+    {
+        //std::cout << Enext << "\t";
+        return Enext;//запись в файл
+    }
+    else
+    {
+        return iterationMethod(e * sin(Enext) + M, Enext, M);
+    }
+}
+
+int main()
+{
+    for (int i = 0; i < 361; i++)
+    {
+        double p = a * (1 - e * e);
+        double teta = excentricToTrue(iterationMethod(e * sin(i * 2 * Pi / 360) + (i * 2 * Pi / 360), i * 2 * Pi / 360, i * 2 * Pi / 360));
+        //std::cout << i << "\t" << i * 2 * Pi / 360 << "\t" << i << "\t" << teta << "\t" << i << "\n";
+        std::cout << radFind(teta, a * (1 - e * e)) << "\t" << speedRad(p, teta) << "\t" << speedN(p, teta) << "\t" << fullSpeed(p, teta) << "\t" << i << std::endl;
+    }
+}
+
